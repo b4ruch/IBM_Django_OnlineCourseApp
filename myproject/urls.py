e2738b44969bba8d90e058ce37ae208c38ddd73d
b4ruch <12469509+b4ruch@users.noreply.github.com>
2021-06-01 21:09:10 -0700

    implements templates, views, routes, admin, and cloud config

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path("", view=views.index)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
